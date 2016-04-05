53f9e0f058bf89741c215c87a8fb012db96b36ae
chenxingyu <wizard_cxy@hotmail.com>
2016-04-05 17:07:59 +0800

    finish pj4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,66 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+// first put the starting address of the screen in R0
+
+
+(START)
+// store the screen start address in R0
+@SCREEN
+D=A
+@R0
+M=D
+
+// get the keyboard input
+@KBD
+D=M
+// if D!=0 jump to black screen part
+@BLACK
+D;JGT
+
+// else jump to white screen part
+@WHITE
+D;JEQ
+
+(BLACK)
+// store the black color in R1
+@R1
+M=-1 // fill the screen with all 1 (1111111111111111)
+
+@UPDATE
+0;JMP
+
+(WHITE)
+// store the white color in R1
+@R1
+M=0
+@UPDATE
+0;JMP
+
+(UPDATE)
+//fill the whole screen with R1 color
+@R1
+D=M
+
+// first get the address which want to color
+@R0
+A=M
+//color it
+M=D
+
+// move to the next chunk of 16-bit pixel
+@R0
+D=M+1
+
+@KBD
+D=A-D
+
+@R0
+M=M+1
+
+//if D>0 still need to fill 
+@UPDATE
+D;JGT
+
+@START
+0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -4,6 +4,38 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+// zero the result
+@R2
+M=0
+
+//set i=0
+@i
+M=0
+
+(LOOP)
+// if (i==R1) goto end
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JEQ
+
+// Add R0 to R2 R1 times
+@R0
+D=M
+@R2
+M=M+D
+
+//i=i+1
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
